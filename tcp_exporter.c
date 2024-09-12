b933643487d5281aa491d3d7a4a49bf744a3fa83
Walter Vargas <waltervargas@linux.com>
2024-09-12 03:29:38 +0200

    Fix stack smash bug

--- tcp_exporter.c
+++ tcp_exporter.c
@@ -8,6 +8,7 @@
 #include <netinet/tcp.h>
 
 #define PORT 9318
+#define INITIAL_BUFFER_SIZE 1024
 
 // Structure to track connections by host and port
 struct connection_metrics {
@@ -21,6 +22,8 @@ struct connection_metrics {
 
 #define MAX_HOSTS 100
 
+// TODO: This cannot be a global variable
+// due the fact that this have to be recomputed on per request.
 struct connection_metrics metrics[MAX_HOSTS];
 int metrics_count = 0;
 
@@ -78,6 +81,27 @@ const char *map_tcp_state_to_prometheus(int tcp_state) {
     }
 }
 
+int append_to_buffer(char **response, size_t *response_size, const char *new_content) {
+    size_t new_content_len = strlen(new_content);
+    size_t current_len = strlen(*response);
+
+    // Check if buffer needs to be resized
+    if (current_len + new_content_len + 1 > *response_size) {
+        size_t new_size = *response_size * 2 + new_content_len;  // Double the buffer size
+        char *new_buffer = realloc(*response, new_size);
+        if (!new_buffer) {
+            fprintf(stderr, "Failed to allocate memory\n");
+            return 1;  // Memory allocation failed
+        }
+        *response = new_buffer;
+        *response_size = new_size;
+    }
+
+    // Append the new content
+    strcat(*response, new_content);
+    return 0;
+}
+
 /**
  * @brief Serves metrics in Prometheus format over HTTP.
  *
@@ -94,37 +118,43 @@ const char *map_tcp_state_to_prometheus(int tcp_state) {
 enum MHD_Result serve_metrics(void *cls, struct MHD_Connection *connection,
                               const char *url, const char *method, const char *version,
                               const char *upload_data, size_t *upload_data_size, void **con_cls) {
-    char response[4096] = {0};
-    char buffer[512];
+    size_t buffer_size = INITIAL_BUFFER_SIZE;
+    char *response = malloc(buffer_size);
+    if (!response) {
+        perror("failed to allocate memory for response");
+        return MHD_NO;
+    }
+    response[0] = '\0';
 
-    // Prometheus metrics template
-    strcat(response,
-           "# HELP conntrack_opening_connections How many connections to the remote host are currently opening?\n"
-           "# TYPE conntrack_opening_connections gauge\n"
-           "# HELP conntrack_open_connections How many open connections are there to the remote host?\n"
-           "# TYPE conntrack_open_connections gauge\n"
-           "# HELP conntrack_closing_connections How many connections to the remote host are currently closing?\n"
-           "# TYPE conntrack_closing_connections gauge\n"
-           "# HELP conntrack_closed_connections How many connections to the remote host have recently closed?\n"
-           "# TYPE conntrack_closed_connections gauge\n");
-
-    // For each host, add its metrics
+       // For each host, add its metrics
     for (int i = 0; i < metrics_count; i++) {
+        char buffer[1024];
         snprintf(buffer, sizeof(buffer),
-                 "conntrack_opening_connections{host=\"%s:%u\"} %d\n"
+                 "# HELP conntrack_opening_connections How many connections to the remote host are currently opening?\n"
+                 "# TYPE conntrack_opening_connections gauge\n"
                  "conntrack_open_connections{host=\"%s:%u\"} %d\n"
+                 "# HELP conntrack_open_connections How many open connections are there to the remote host?\n"
+                 "# TYPE conntrack_open_connections gauge\n"
+                 "conntrack_opening_connections{host=\"%s:%u\"} %d\n"
+                 "# HELP conntrack_closing_connections How many connections to the remote host are currently closing?\n"
+                 "# TYPE conntrack_closing_connections gauge\n"
                  "conntrack_closing_connections{host=\"%s:%u\"} %d\n"
+                 "# HELP conntrack_closed_connections How many connections to the remote host have recently closed?\n"
+                 "# TYPE conntrack_closed_connections gauge\n"
                  "conntrack_closed_connections{host=\"%s:%u\"} %d\n",
                  metrics[i].host, metrics[i].port, metrics[i].opening_connections,
                  metrics[i].host, metrics[i].port, metrics[i].open_connections,
                  metrics[i].host, metrics[i].port, metrics[i].closing_connections,
                  metrics[i].host, metrics[i].port, metrics[i].closed_connections);
 
-        strcat(response, buffer);
+        if (append_to_buffer(&response, &buffer_size, buffer)) {
+            free(response);
+            return MHD_NO;
+        }
     }
 
     struct MHD_Response *http_response;
-    http_response = MHD_create_response_from_buffer(strlen(response), (void *)response, MHD_RESPMEM_PERSISTENT);
+    http_response = MHD_create_response_from_buffer(strlen(response), (void *)response, MHD_RESPMEM_MUST_FREE);
     enum MHD_Result ret = MHD_queue_response(connection, MHD_HTTP_OK, http_response);
     MHD_destroy_response(http_response);
     return ret;
@@ -137,7 +167,7 @@ enum MHD_Result serve_metrics(void *cls, struct MHD_Connection *connection,
  */
 int start_http_server() {
     struct MHD_Daemon *daemon;
-    daemon = MHD_start_daemon(MHD_USE_SELECT_INTERNALLY, PORT, NULL, NULL, &serve_metrics, NULL, MHD_OPTION_END);
+    daemon = MHD_start_daemon(MHD_USE_INTERNAL_POLLING_THREAD, PORT, NULL, NULL, &serve_metrics, NULL, MHD_OPTION_END);
     if (NULL == daemon) {
         return 1;
     }
@@ -198,7 +228,10 @@ static int event_callback(enum nf_conntrack_msg_type type, struct nf_conntrack *
  * @return EXIT_SUCCESS on success, EXIT_FAILURE on failure.
  */
 int main() {
-    start_http_server();
+    if (start_http_server() == 1) {
+        perror("unable to start http server");
+        return EXIT_FAILURE;
+    }
 
     struct nfct_handle *handle;
     handle = nfct_open(CONNTRACK, NF_NETLINK_CONNTRACK_UPDATE);
